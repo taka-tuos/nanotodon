dda4dde710ea4f8088dd514764a2a3e9e9fd7ea3
shibafu <shibafu528@gmail.com>
2020-01-13 23:58:35 +0900

    fix root_dir fallback flow

--- config.c
+++ config.c
@@ -4,38 +4,64 @@
 #include <errno.h>
 #include "config.h"
 
-static int mkdir_or_die(const char* path);
+#define MAKE_CONFIG_DIR_OK 0x00
+#define MAKE_CONFIG_DIR_CANT_CREATE 0x01
+#define MAKE_CONFIG_DIR_FILE_EXISTS 0x02
+
+static int make_config_dir(const char* path);
+static void make_config_dir_or_die(const char* path);
 static int init_xdg(struct nanotodon_config *config, const char* xdg_config_home);
 
-static int mkdir_or_die(const char* path)
+static int make_config_dir(const char* path)
 {
 	// existence check
 	struct stat st;
 	if (stat(path, &st) == 0) {
 		if ((st.st_mode & S_IFMT) == S_IFDIR) {
-			return 1;
+			return MAKE_CONFIG_DIR_OK;
 		}
 
 		// exists other entry
-		fprintf(stderr, "FATAL: File already exists '%s'\n", path);
-		exit(EXIT_FAILURE);
+		return MAKE_CONFIG_DIR_FILE_EXISTS;
 	}
 
 	if (errno == ENOENT && mkdir(path, 0755) == 0) {
-		return 1;
+		return MAKE_CONFIG_DIR_OK;
 	}
 
-	fprintf(stderr, "FATAL: Can't create directory '%s' (errno=%d)\n", path, errno);
-	exit(EXIT_FAILURE);
+	return MAKE_CONFIG_DIR_CANT_CREATE;
+}
+
+static void make_config_dir_or_die(const char* path)
+{
+	int result = make_config_dir(path);
+
+	switch (result)
+	{
+	case MAKE_CONFIG_DIR_OK:
+		return;
+	case MAKE_CONFIG_DIR_CANT_CREATE:
+		fprintf(stderr, "FATAL: Can't create directory '%s' (errno=%d)\n", path, errno);
+		exit(EXIT_FAILURE);
+	case MAKE_CONFIG_DIR_FILE_EXISTS:
+		fprintf(stderr, "FATAL: File already exists '%s'\n", path);
+		exit(EXIT_FAILURE);
+	default:
+		fprintf(stderr, "FATAL: Undefined error (errno=%d)\n", errno);
+		exit(EXIT_FAILURE);
+	}
 }
 
 static int init_xdg(struct nanotodon_config *config, const char* xdg_config_home)
 {
 	// make xdg config home
-	mkdir_or_die(xdg_config_home);
-	sprintf(config->root_dir, "%s/nanotodon", xdg_config_home);
+	int make_home_result = make_config_dir(xdg_config_home);
+	if (make_home_result != MAKE_CONFIG_DIR_OK) {
+		return make_home_result;
+	}
 
-	return 1;
+	sprintf(config->root_dir, "%s/nanotodon", xdg_config_home);
+	return make_config_dir(config->root_dir);
 }
 
 int nano_config_init(struct nanotodon_config *config)
@@ -45,29 +71,23 @@ int nano_config_init(struct nanotodon_config *config)
 	// TODO: マクロでXDG Base Directory使うか切り替えてもよさそう
 	// XDG_CONFIG_HOME
 	char *xdg_config_home = getenv("XDG_CONFIG_HOME");
-	if (xdg_config_home != NULL) {
-		if (init_xdg(config, xdg_config_home)) {
-			goto makepath;
-		} else {
-			return 0;
-		}
+	if (xdg_config_home != NULL && init_xdg(config, xdg_config_home) == MAKE_CONFIG_DIR_OK) {
+		goto makepath;
 	}
 
 	// $HOME/.config
 	char default_xdg_config_home[256];
 	sprintf(default_xdg_config_home, "%s/.config", homepath);
-	if (init_xdg(config, default_xdg_config_home)) {
+	if (init_xdg(config, default_xdg_config_home) == MAKE_CONFIG_DIR_OK) {
 		goto makepath;
-	} else {
-		return 0;
 	}
 
 	// $HOME/.nanotodon
 	sprintf(config->root_dir, "%s/.nanotodon", homepath);
+	make_config_dir_or_die(config->root_dir);
 
 makepath:
 
-	mkdir_or_die(config->root_dir);
 	sprintf(config->dot_token, "%s/token", config->root_dir);
 	sprintf(config->dot_domain, "%s/domain", config->root_dir);
 
@@ -78,7 +98,7 @@ int nano_config_app_token_filename(struct nanotodon_config *config, const char* 
 {
 	char app_token_dir[256];
 	snprintf(app_token_dir, sizeof(app_token_dir), "%s/app_token", config->root_dir);
-	mkdir_or_die(app_token_dir);
+	make_config_dir_or_die(app_token_dir);
 
 	return snprintf(buf, buf_length, "%s/%s", app_token_dir, domain);
 }
