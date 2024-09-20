9dfc9ed1616d316662eaf08ac20ab49d26a9f04f
taka-tuos <hvcnes@gmail.com>
2024-09-20 11:37:29 +0900

    画像サイズ拡大 #68

--- sixel.h
+++ sixel.h
@@ -3,8 +3,8 @@
 
 #include "sbuf.h"
 
-#define SIXEL_MUL_PIC 24
-#define SIXEL_MUL_ICO 4
+#define SIXEL_MUL_PIC 48
+#define SIXEL_MUL_ICO 8
 
 void print_picture(sbctx_t *sbctx, char *uri, int mul);
 void sixel_init(void);
