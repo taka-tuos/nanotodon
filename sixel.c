901cf13571a815fb32da6dc0b0e8ef57921ad1e8
Izumi Tsutsui <tsutsui@ceres.dti.ne.jp>
2024-11-21 00:32:51 +0900

    stb_imageの画像形式のうちmastodonで不要なものを無効にする

--- sixel.c
+++ sixel.c
@@ -10,6 +10,12 @@
 
 #ifdef USE_SIXEL
 #define STB_IMAGE_IMPLEMENTATION
+#define STBI_NO_BMP
+#define STBI_NO_PSD
+#define STBI_NO_TGA
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
 #include "stb_image.h"
 
 #include "err.png.h"
